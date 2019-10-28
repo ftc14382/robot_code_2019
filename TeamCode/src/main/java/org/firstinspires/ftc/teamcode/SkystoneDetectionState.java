88eb43abbdec98285e2fbc01f35dbf98a0d1f77c
Owner <bhargrove09@gmail.com>
2019-10-28 16:04:02 -0400

    I added a variable for the actual position of the detected object on the display.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionState.java
@@ -15,6 +15,7 @@ package org.firstinspires.ftc.teamcode;
 public class SkystoneDetectionState {
     public boolean detected       = false;
     public int     detectedState = 0;
+    public int     detectedPosition = 0;
     public String  telemetry1    = "";
     public String  telemetry2    = "";
 }
