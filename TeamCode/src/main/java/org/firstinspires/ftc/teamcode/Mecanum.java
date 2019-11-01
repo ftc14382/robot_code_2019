62dda40a548f1b18058e5adedd8ed4e91727a224
Owner <bhargrove09@gmail.com>
2019-10-31 20:27:06 -0400

    Added useful functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -7,8 +7,14 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
 
 public class Mecanum {
+    public IMU iMU;
     public ElapsedTime runtime = new ElapsedTime();
     public DcMotor leftFront = null;
     public DcMotor leftBack = null;
@@ -28,6 +34,7 @@ public class Mecanum {
 
     public void init(HardwareMap ahwMap, LinearOpMode Arobot) {
         robot = Arobot;
+        iMU.init(ahwMap, Arobot);
         leftFront = ahwMap.get(DcMotor.class, "left_front");
         leftBack = ahwMap.get(DcMotor.class, "left_back");
         rightFront = ahwMap.get(DcMotor.class, "right_front");
@@ -49,70 +56,131 @@ public class Mecanum {
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
     public void simpleDrive(double distance, double dir) {
-        leftFront.setPower(dir);
-        leftBack.setPower(dir);
-        rightFront.setPower(dir);
-        rightBack.setPower(dir);
-        if (robot.opModeIsActive()) {
-            robot.telemetry.addData("Path", "Running forwards %2d inches", distance);
-            robot.telemetry.update();
-            while (robot.opModeIsActive() &&
-                    leftFront.getCurrentPosition() < distance * COUNTS_PER_INCH) {
-                robot.telemetry.addData("Path", "Running. . .");
-                robot.telemetry.update();
-            }
-            leftFront.setPower(0);
-            leftBack.setPower(0);
-            rightFront.setPower(0);
-            rightBack.setPower(0);
+        int lFTarget;
+        int lBTarget;
+        int rFTarget;
+        int rBTarget;
+        if(robot.opModeIsActive()) {
+            lFTarget = leftFront.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+            lBTarget = leftBack.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+            rFTarget = rightFront.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+            rBTarget = rightBack.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
 
-            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftFront.setTargetPosition(lFTarget);
+            leftBack.setTargetPosition(lBTarget);
+            rightFront.setTargetPosition(rFTarget);
+            rightBack.setTargetPosition(rBTarget);
+
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftFront.setPower(dir);
+            leftBack.setPower(dir);
+            rightFront.setPower(dir);
+            rightBack.setPower(dir);
+
         }
+        robot.telemetry.addData("Path", "Running forwards %2d inches", distance);
+        robot.telemetry.update();
+        while (robot.opModeIsActive() && leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack.isBusy()) {
+            robot.telemetry.addData("Path", "Running. . .");
+            robot.telemetry.update();
+        }
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 
-    public void turn(double degrees, int dir) {
-        leftFront.setPower(dir);
-        leftBack.setPower(dir);
-        rightFront.setPower(-dir);
-        rightBack.setPower(-dir);
+    public void turn(double degrees, double dir) {
+        int lFTarget;
+        int lBTarget;
+        int rFTarget;
+        int rBTarget;
+        if(robot.opModeIsActive()) {
+            lFTarget = leftFront.getCurrentPosition() - (int)(degrees * COUNTS_PER_DEGREE);
+            lBTarget = leftBack.getCurrentPosition() - (int)(degrees * COUNTS_PER_DEGREE);
+            rFTarget = rightFront.getCurrentPosition() + (int)(degrees * COUNTS_PER_DEGREE);
+            rBTarget = rightBack.getCurrentPosition() + (int)(degrees * COUNTS_PER_DEGREE);
 
-        if (robot.opModeIsActive()) {
-            robot.telemetry.addData("Path", "Turning %7d degrees", degrees);
-            robot.telemetry.update();
+            leftFront.setTargetPosition(lFTarget);
+            leftBack.setTargetPosition(lBTarget);
+            rightFront.setTargetPosition(rFTarget);
+            rightBack.setTargetPosition(rBTarget);
 
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            while (robot.opModeIsActive() &&
-                    leftFront.getCurrentPosition() < COUNTS_PER_DEGREE * degrees){
-                robot.telemetry.addData("Path", "Turning. . .");
-                robot.telemetry.update();
-            }
-            leftFront.setPower(0);
-            leftBack.setPower(0);
-            rightFront.setPower(0);
-            rightBack.setPower(0);
+            leftFront.setPower(dir);
+            leftBack.setPower(dir);
+            rightFront.setPower(dir);
+            rightBack.setPower(dir);
 
-
-
-            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+
+        robot.telemetry.addData("Path", "Turning %7d degrees", degrees);
+        robot.telemetry.update();
+
+
+        while (robot.opModeIsActive() && leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack.isBusy()){
+            robot.telemetry.addData("Path", "Turning. . .");
+            robot.telemetry.update();
+        }
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
-    public void sideDrive(double distance, double dir) {
-        leftFront.setPower(dir);
-        leftBack.setPower(-dir);
-        rightFront.setPower(dir);
-        rightBack.setPower(-dir);
-        if (robot.opModeIsActive()) {
+
+    public void sideDrive(double distance, double dir) {//Positive is to the left
+        int lFTarget;
+        int lBTarget;
+        int rFTarget;
+        int rBTarget;
+        if(robot.opModeIsActive()) {
+            lFTarget = leftFront.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+            lBTarget = leftBack.getCurrentPosition() - (int)(distance * COUNTS_PER_INCH);
+            rFTarget = rightFront.getCurrentPosition() - (int)(distance * COUNTS_PER_INCH);
+            rBTarget = rightBack.getCurrentPosition() + (int)(distance * COUNTS_PER_INCH);
+
+            leftFront.setTargetPosition(lFTarget);
+            leftBack.setTargetPosition(lBTarget);
+            rightFront.setTargetPosition(rFTarget);
+            rightBack.setTargetPosition(rBTarget);
+
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftFront.setPower(dir);
+            leftBack.setPower(dir);
+            rightFront.setPower(dir);
+            rightBack.setPower(dir);
+
+        }
             robot.telemetry.addData("Path", "Moving %2d inches sideways", distance);
             robot.telemetry.update();
 
-            while (robot.opModeIsActive() &&
-                    leftFront.getCurrentPosition() < COUNTS_PER_INCH * distance){
+            while (robot.opModeIsActive() && leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack.isBusy()){
                 robot.telemetry.addData("Path", "Crab Walking. . .");
                 robot.telemetry.update();
             }
@@ -125,8 +193,67 @@ public class Mecanum {
             leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
+
     }
+
+    public void driveTo(RobotInfo r, Position p, boolean brake) {
+        double deltaX = p.x - r.x;
+        double deltaY = p.y - r.y;
+        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        double theta = Math.atan2(deltaY, deltaX);
+        double turn = Math.toDegrees(theta) - r.degrees;
+        double startIMUangle = getIMUAngle();//for loging
+        double IMUTurned;
+
+        //RobotLog.ii(Tag, "driveTo: initial(x,y,degrees): %.2f, %.2f, %.2f", r.x, r.y, r.degrees);
+        //RobotLog.ii(Tag, "driveTo: target(x,y,degrees): %.2f, %.2f", p.x, p.y);
+
+        if (turn > 180) {
+            turn -= 360;
+        }
+        if (turn < -180) {
+            turn += 360;
+        }
+        //RobotLog.ii(Tag, "DriveTo: comanded angle: %.2f", turn);
+        /*if(brake == true) {
+            robot.leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }*/
+
+
+        turn(turn, 0.5);
+        simpleDrive(distance, 0.7);
+
+        /*if(brake == true) {
+            robot.leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        }*/
+        IMUTurned = getIMUField();
+
+        r.x = p.x;
+        r.y = p.y;
+        if(Math.abs(IMUTurned - Math.toDegrees(theta)) < 9) {
+            r.degrees = getIMUField();
+        } else {
+            r.degrees = Math.toDegrees(theta);
+        }
+
+        /*telemetry.addData("RobotX:", r.x);
+        telemetry.addData("RobotY", r.y);
+        telemetry.addData("Robot Heading", r.degrees);*/
+        //RobotLog.ii(Tag, "DriveTo: turned IMU angle: %.2f", getIMUAngle() - startIMUangle);
+    }
+
+    public double getIMUAngle() {
+        return -iMU.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+    }
+
+    public double getIMUField() {
+        double actual = getIMUAngle() + iMU.startIMUOffset;
+        //RobotLog.ii(Tag, "getIMUField: returned angle: %.2f", actual);
+        return(actual);
+    }
+
     public void encoderDrive(double upInches, double rightInches) {
         double distance = Math.hypot(upInches, rightInches);
         double robotAngle = Math.atan2(upInches, rightInches) + Math.PI / 4;
