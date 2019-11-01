eb933dd52e177e89f1bec396b1ba2002b540217c
Owner <bhargrove09@gmail.com>
2019-10-31 20:27:57 -0400

    This initializes the IMU and contains all the IMU basically needs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMU.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class IMU {
+    // The IMU sensor object
+    BNO055IMU imu;
+    public double startIMUOffset;
+
+
+
+
+    public void init(HardwareMap ahwMap, LinearOpMode Arobot) {
+        BNO055IMU.Parameters parameters2 = new BNO055IMU.Parameters();
+        parameters2.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters2.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters2.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters2.loggingEnabled = true;
+        parameters2.loggingTag = "IMU";
+        parameters2.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = ahwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters2);
+    }
+}
