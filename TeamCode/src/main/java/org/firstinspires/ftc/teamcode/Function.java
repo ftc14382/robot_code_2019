735fc5cbc232bbb73a0f8d0b5defb621f4c89171
Owner <bhargrove09@gmail.com>
2019-11-20 17:52:22 -0500

    I added the servo to move the foundation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Function.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Function.java
@@ -3,14 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Function {
     public DcMotor lifter;
     public DcMotor grabber;
+    public Servo foundMover;
 
     public void init(HardwareMap ahwMap) {
         lifter = ahwMap.get(DcMotor.class, "lifter");
         grabber = ahwMap.get(DcMotor.class, "grabber");
+        foundMover = ahwMap.get(Servo.class, "found_servo");
 
         lifter.setDirection(DcMotor.Direction.FORWARD);
         grabber.setDirection(DcMotor.Direction.FORWARD);
